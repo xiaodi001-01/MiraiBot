4c8e7a41f0c5bb860614b5bd5508c317e72850cb
xiaodi001 <126065840+xiaodi001-01@users.noreply.github.com>
2023-10-16 16:13:54 +0800

    Update Bot.py
    add some code(test)

--- Bot.py
+++ Bot.py
@@ -1,7 +1,6 @@
 import requests
-
-from DiMirai.utils import commands
-from DiMirai.core import messageProcess
-host=3292744510 # 写你的QQ号
-messageProcess.test(host,'127.0.0.1','5700')
-
+ip=input(输入提供的IP+端口,不填写为127.0.0.1:5700)
+if ip==null:
+  ip="http://127.0.0.1:5700"
+#json={}
+#test=requests.post(ip,json=json)
